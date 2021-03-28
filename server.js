aee3371925e181b3c6f762ff5dd840b08fa2b698
zakeyah <mom11omar@gmail.com>
2021-03-28 19:41:03 +0300

    location done

--- server.js
+++ server.js
@@ -1,7 +1,31 @@
+'use strict';
 const express = require('express');
+const cosr = require('cors');
+require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+app.use(cosr());
+
+const handelLocation = (request,response)=>{
+  const locationFolder =require('./data/location.json');
+  const locationInfo = new Location (locationFolder[0]);
+  response.json(locationInfo);
+};
+app.get('/location',handelLocation);
+
+
+function Location(info) {
+  this.search_query = info.display_name.split(',')[0];
+  this.formatted_query =info.display_name;
+  this.latitude = info.lat;
+  this.longitude = info.lon;
+}
+
+
+
+
+
 
 const handleRequest = (request, response) => {
   console.log(request.query);
